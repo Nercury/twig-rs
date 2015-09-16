ca04dae33ac778a1c97bc42fcebc6a3021ec2593
Nerijus Arlauskas <nercury@gmail.com>
2015-09-16 19:07:14 +0300

    Use the correct license.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+/**
+ * This module is part of twig-rs.
+ *
+ * (c) 2015 Rust Twig Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 extern crate regex;
 
 pub mod lexer;
