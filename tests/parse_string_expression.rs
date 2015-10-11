b8f4847163ff2e2f2cc8a6deb75243c290bb7ba2
Nerijus Arlauskas <nercury@gmail.com>
2015-10-11 16:51:23 +0300

    Pass concatenate_two_consecutive_strings test.

--- tests/parse_string_expression.rs
+++ tests/parse_string_expression.rs
@@ -5,18 +5,20 @@ use twig::Environment;
 use twig::Lexer;
 use twig::node::Module;
 use twig::parser::{ Context, Parse };
+use twig::Result;
+
+#[test]
+#[should_panic(
+    expected = r#"Unexpected token "string" of value "b" ("end of print statement" expected)"#
+)]
+fn test_string_expression_does_not_concatenate_two_consecutive_strings() {
+    maybe_parsed(r#"{{ "a" "b" }}"#).unwrap();
+}
 
 #[test]
 fn test_string_expression() {
     for (template, expected) in get_tests_for_string() {
-        let env = Environment::default().init_all();
-        let lexer = Lexer::default(&env.lexing);
-        let mut tokens = lexer.tokens(&template);
-        let mut context = Context::new(&env.parsing, &mut tokens);
-        let module = match Module::parse(&mut context) {
-            Ok(m) => m,
-            Err(e) => panic!("parsing error: {:?}", e),
-        };
+        let module = expect_parsed(template);
         assert_eq!(module.body.expect_print(), &expected);
     }
 }
@@ -49,4 +51,19 @@ fn get_tests_for_string<'r>() -> Vec<(&'static str, Expr<'r>)> {
             right: Box::new(Expr::new_str_constant(" baz", 1)),
         }, 1)),
     ]
+}
+
+fn maybe_parsed(template: &'static str) -> Result<Module> {
+    let env = Environment::default().init_all();
+    let lexer = Lexer::default(&env.lexing);
+    let mut tokens = lexer.tokens(template);
+    let mut context = Context::new(&env.parsing, &mut tokens);
+    Module::parse(&mut context)
+}
+
+fn expect_parsed(template: &'static str) -> Module {
+    match maybe_parsed(template) {
+        Ok(m) => m,
+        Err(e) => panic!("parsing error: {:?}", e),
+    }
 }
