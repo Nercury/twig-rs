676fe01a5bbc961ec69836eb79b55ef81873168e
Nerijus Arlauskas <nercury@gmail.com>
2015-10-17 15:46:17 +0300

    Begin escaper extension.

---
+++ src/extension/escaper/mod.rs
@@ -0,0 +1,10 @@
+use Environment;
+use extension::Extension;
+
+pub struct EscaperExtension;
+
+impl Extension for EscaperExtension {
+    fn apply(env: &mut Environment) {
+        
+    }
+}
--- src/extension/mod.rs
+++ src/extension/mod.rs
@@ -1,8 +1,10 @@
 mod core;
+mod escaper;
 
 use Environment;
 
 pub use self::core::CoreExtension;
+pub use self::escaper::EscaperExtension;
 
 /// Implement this trait to create a new Twig extension.
 pub trait Extension {
