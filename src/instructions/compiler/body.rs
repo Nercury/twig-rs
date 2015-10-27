fd683f115443c99641f56df1d49d24a6535731a7
Nerijus Arlauskas <nercury@gmail.com>
2015-10-27 20:59:54 +0200

    Add traces.

--- src/instructions/compiler/body.rs
+++ src/instructions/compiler/body.rs
@@ -6,8 +6,10 @@ use error::TemplateResult;
 
 impl<'c> Compile for Body<'c> {
     fn compile(&self, template: &mut Template<Value>) -> TemplateResult<()> {
+        trace!("Body::compile");
         match *self {
             Body::List { ref items } => {
+                trace!("List::compile");
                 for item in items {
                     try!(item.compile(template));
                 }
--- src/instructions/compiler/module.rs
+++ src/instructions/compiler/module.rs
@@ -6,6 +6,7 @@ use error::TemplateResult;
 
 impl<'c> Compile for Module<'c> {
     fn compile(&self, template: &mut Template<Value>) -> TemplateResult<()> {
+        trace!("Module::compile");
         self.body.compile(template)
     }
 }
